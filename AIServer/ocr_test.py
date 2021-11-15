991222700b752f16931aca5348176e421c7db214
hiseoung <anhisg4104@gmail.com>
2021-11-16 00:41:40 +0900

    Update OCR_TEST

--- AIServer/ocr_test.py
+++ AIServer/ocr_test.py
@@ -1,5 +1,32 @@
 import easyocr
-reader = easyocr.Reader(['ch_sim','en'])
-result = reader.readtext('test.jpg')
-print(result)
+import cv2
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from PIL import ImageFont, ImageDraw, Image
 
+reader = easyocr.Reader(['en']) 
+result = reader.readtext('C:/Users/anhis/Desktop/VSCodeProjects/PaperReadingHelper/AIServer/test.png')
+img = cv2.imread('C:/Users/anhis/Desktop/VSCodeProjects/PaperReadingHelper/AIServer/test.png')
+
+img = Image.fromarray(img)
+font = ImageFont.truetype("fonts/HMKMRHD.TTF", 20)
+draw = ImageDraw.Draw(img)
+
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(255,3), dtype="uint8")
+
+for i in result:
+    x = i[0][0][0]
+    y = i[0][0][1]
+    w = i[0][1][0] - i[0][0][0]
+    h = i[0][2][1] - i[0][1][1]
+
+    color_idx = 180
+    color = [int(c) for c in COLORS[color_idx]]
+
+    draw.rectangle(((x, y), (x+w, y+h)), outline=tuple(color), width=2)
+
+plt.imshow(img)
+plt.show()
+plt.close()
